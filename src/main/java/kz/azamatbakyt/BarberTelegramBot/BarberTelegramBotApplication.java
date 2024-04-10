82f5b896e7ccc575cb6a9701346f352e8c7bdad1
azamatbakyt <azamatbakit02@mail.ru>
2024-04-10 20:52:10 +0500

    remove useless classes

--- src/main/java/kz/azamatbakyt/BarberTelegramBot/BarberTelegramBotApplication.java
+++ src/main/java/kz/azamatbakyt/BarberTelegramBot/BarberTelegramBotApplication.java
@@ -1,20 +1,12 @@
 package kz.azamatbakyt.BarberTelegramBot;
 
-import kz.azamatbakyt.BarberTelegramBot.service.AppointmentService;
-import kz.azamatbakyt.BarberTelegramBot.service.AppointmentTimeslotService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BarberTelegramBotApplication{
 
-    private final AppointmentService appointmentService;
-    private  final AppointmentTimeslotService appointmentTimeslotService;
 
-    public BarberTelegramBotApplication(AppointmentService appointmentService, AppointmentTimeslotService appointmentTimeslotService) {
-        this.appointmentService = appointmentService;
-        this.appointmentTimeslotService = appointmentTimeslotService;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(BarberTelegramBotApplication.class, args);
